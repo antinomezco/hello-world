6d14cffb40564d6706ed527a3a1bd2627ac26552
antinomezco <45720754+antinomezco@users.noreply.github.com>
2019-03-11 21:51:36 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,3 @@
 def hello():
-    return "Hello, World!" #testing the addition of comments
+    """This is a sample program that just prints out 'Hello, World!'"""
+    return "Hello, World!" #Prints out the text in quotes after 'return'
